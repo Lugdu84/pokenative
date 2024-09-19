ea95fc190918b84f60a90fd4ce3f6c63c211aa1d
David Grammatico <david.lugdunum@gmail.com>
2024-09-19 19:46:18 +0200

    Refactor fetching logic in HomeScreen component and add SortButton and Radio components for sorting functionality

--- src/app/pokemon/[id].tsx
+++ src/app/pokemon/[id].tsx
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 
 export default function PokemonDetailScreen() {
 	const { id } = useLocalSearchParams<{ id: string }>();
+
 	return (
 		<View>
 			<Text>Detail du pokemon : {id}</Text>
